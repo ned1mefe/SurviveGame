a520cc63448f077f60e3ef99d5994345565205c3
Nedim Efe Candaş <efemm9@gmail.com>
2025-01-31 20:08:31 +0300

    add bullet range

--- Assets/Scripts/Weapons/Projectiles/Projectile.cs
+++ Assets/Scripts/Weapons/Projectiles/Projectile.cs
@@ -6,8 +6,9 @@ namespace Weapons.Projectiles
     public abstract class Projectile : MonoBehaviour
     {
         [SerializeField] private int damage;
-        [SerializeField] private int range;
+        [SerializeField] private float range;
         [SerializeField] private int speed;
+        private float _startTime;
         private Rigidbody2D _rb;
         private void Awake()
         {
@@ -16,8 +17,25 @@ namespace Weapons.Projectiles
 
         private void Start()
         {
+            _startTime = Time.time;
             Vector3 direction = transform.up;
             _rb.velocity = new Vector2(direction.x * speed, direction.y * speed);
+        }
+
+        private void FixedUpdate()
+        {
+            CheckRange();
+        }
+
+        private void CheckRange()
+        {
+            float elapsedTime = Time.time - _startTime;
+            float traveledDistance = speed * elapsedTime;
+
+            if (traveledDistance >= range)
+            {
+                Destroy(gameObject);
+            }
         }
 
         protected void OnTriggerEnter2D(Collider2D other)
